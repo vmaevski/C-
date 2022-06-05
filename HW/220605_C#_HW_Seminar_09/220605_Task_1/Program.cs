b121703896d46a14e08de3b806432843e867e07a
vmaevski <vmaevski@mail.ru>
2022-06-05 19:04:03 +0700

    Task 1 completed.

---
+++ HW/220605_C#_HW_Seminar_09/220605_Task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// 1. Задайте числа M и N. Напишите программу, которая выведет
+// все чётные натуральные числа от M до N с помощью рекурсии.
+
+ void EvenNaturalNumbers(int m, int n)
+{
+    n = n/2 * 2;
+    if (n < m) return;
+    EvenNaturalNumbers(m, n - 2);
+    Console.Write($"{n}, ");
+}
+ 
+EvenNaturalNumbers(1, 11);
