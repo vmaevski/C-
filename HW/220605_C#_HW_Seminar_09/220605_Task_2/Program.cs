fbb0dcf0840816de421e3b13ffe147ecd1f8cdbc
vmaevski <vmaevski@mail.ru>
2022-06-05 19:36:14 +0700

    Task 2 completed.

---
+++ HW/220605_C#_HW_Seminar_09/220605_Task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// 2. Задайте числа M и N. Напишите программу, которая найдёт
+// сумму натуральных элементов от M до N с помощью рекурсии.
+
+ int SumNaturalNumbers(int m, int n)
+{
+    if (n < m) return 0;
+    return n + SumNaturalNumbers(m, n - 1);
+}
+ 
+Console.WriteLine($"Сумма = {SumNaturalNumbers(1, 15)}");
+Console.WriteLine($"Сумма = {SumNaturalNumbers(4, 8)}");
